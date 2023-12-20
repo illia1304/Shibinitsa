be6bc766084cbd7a68c4f4b4378ffcc47b6f19a7
Illia <1304illia@gmail.com>
2023-12-20 19:29:42 +0200

    add mistakes

--- Shibinitsa/Program.cs
+++ Shibinitsa/Program.cs
@@ -35,6 +35,10 @@ namespace Hangman
                         symbols[i] = guess;
                         guessCount++;
                     }
+                    else
+                    {
+                        mistakeCount++;
+                    }
                 }
                 if (guessCount == word.Length)
                 {

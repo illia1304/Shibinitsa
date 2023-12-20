f78d4c88bc24cc0ddee06fb39270b060178894ce
Illia <1304illia@gmail.com>
2023-12-20 19:38:59 +0200

    edit

--- Shibinitsa/Program.cs
+++ Shibinitsa/Program.cs
@@ -1,59 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Hangman
+﻿internal class Program
 {
-    class Program
+    private static void Main(string[] args)
     {
-        private static void Main(string[] args)
+        string[] words = { "cat", "money", "pancake", "notebook", "dog", "horse", "tree", "house", "car", "apple", "banana", "orange" };
+        int index = new Random().Next(words.Length - 1);
+        string word = words[index];
+        bool game = true;
+
+
+
+        char[] symbols = new char[word.Length];
+
+        for (int i = 0; i < word.Length; i++)
         {
+            symbols[i] = '*';
+        }
+        Console.WriteLine(symbols);
 
-            List<string> words = new List<string> { "cat", "money", "pancake", "notebook", "dog", "horse", "tree", "house", "car", "apple", "banana", "orange"};
-            int index = new Random().Next(words.Count);
-            string word = words[index];
+        int mistakeCount = 0, guessCount = 0;
+        while (game)
+        {
+            char guess = char.Parse(Console.ReadLine());
 
-            char[] symbols = new char[word.Length];
             for (int i = 0; i < word.Length; i++)
             {
-                symbols[i] = '*';
+                if (word[i] == guess)
+                {
+                    symbols[i] = guess;
+                    guessCount++;
+                    Console.WriteLine(symbols);
+                }
+                else
+                {
+                    mistakeCount++;
+                }
             }
-            Console.WriteLine(symbols);
-            int mistakeCount = 0;
-            int guessCount = 0;
 
-            while (true)
+            if (guessCount == word.Length)
             {
-                Console.WriteLine("Enter your guess:");
-                char guess = char.Parse(Console.ReadLine());
-                for (int i = 0; i < word.Length; i++)
-                {
-                    if (word[i] == guess)
-                    {
-                        symbols[i] = guess;
-                        guessCount++;
-                    }
-                    else
-                    {
-                        mistakeCount++;
-                    }
-                }
-                if (guessCount == word.Length)
-                {
-                    Console.WriteLine("Congratulations! You guessed the word!");
-                    break;
-                }
-                else if (mistakeCount == 5)
-                {
-                    Console.WriteLine("You lose! The correct word was: " + word);
-                    break;
-                }
-
-                Console.WriteLine("Word: " + new string(symbols));
-                Console.WriteLine("Number of mistakes: " + mistakeCount);
+                Console.WriteLine("You win");
+                game = false;
+            }
+            else if(mistakeCount == 5)
+            {
+                Console.WriteLine("You lose");
             }
         }
     }
+
 }
